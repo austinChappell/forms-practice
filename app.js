cadf5201a06b8b2d4f07a2d50e52f767febc90d8
austinChappell <Austin.W.Chappell@gmail.com>
2017-07-27 10:18:11 -0500

    done

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 const express = require('express');
 const mustacheExpress = require('mustache-express');
+const bodyParser = require('body-parser');
 const app = express();
 
 let port = process.env.PORT || 3000;
 
-app.engine('mustache', mustacheExpress());
+let mustacheInstance = mustacheExpress();
+mustacheInstance.cache = null;
+app.engine('mustache', mustacheInstance);
 
 app.set('view engine', 'mustache');
+app.set('views', __dirname + '/views');
 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true }));
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.post('/sayhello', (req, res) => {
+  let data = req.body;
+  res.render('form-submit', data);
 });
 
 app.listen(port, () => {
